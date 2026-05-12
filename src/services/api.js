0643bd0fc90eb3a4fb739b17034ae3962ad6f126
Mohit040206 <mohitkumarpath@gmail.com>
2026-05-12 23:27:59 +0530

    Added the missing backend base uri

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://credza-frontend.vercel.app',
+  baseURL: 'https://credza-backend.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
